29c00c99d445baab7bbb5b431b06e8a3531e1755
Jhonny Mendoza <jhomen11@gmail.com>
2022-08-29 16:41:47 -0400

    configuracion del store y authSlice OK

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import JournalRoutes from "../journal/routes/JournalRoutes";
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
+    
       <Routes>
         {/* Login y registro */}
         <Route path="/auth/*" element={<AuthRoutes />} />
@@ -12,6 +12,5 @@ export const AppRouter = () => {
         {/* JuornalApp */}
         <Route path="/*" element={<JournalRoutes />} />
       </Routes>
-    </BrowserRouter>
   );
 };
